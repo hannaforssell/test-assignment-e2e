3f7f7dc395b83f4f35c849ee4ff34d321be808e6
hannaforssell <forssellhanna@gmail.com>
2023-02-07 17:05:45 +0100

    Adds tests in movies.cy.ts

--- cypress/e2e/movies.cy.ts
+++ cypress/e2e/movies.cy.ts
@@ -2,8 +2,41 @@ beforeEach(() => {
   cy.visit('/');
 });
 
-describe('Input', () => {
-  it('types in input box', () => {
+describe('Finding the elements on start page', () => {
+  it('should find empty input', () => {
+    cy.get('input').should('contain', '');
+  })
+
+  it('should find search button', () => {
+    cy.get('button').should('contain', 'Sök');
+  })
+})
+
+describe('Makes sure input & search button works correctly', () => {
+  it('should type in input box', () => {
     cy.get('input').type('Hello world').should('have.value', 'Hello world');
+  })
+
+  it('should press search button', () => {
+    cy.get('button').click();
+  })
+
+  it('should search for movies titles containing \'Hello world\'', () => {
+    cy.get('input').type('Hello world');
+
+    cy.get('button').click();
+  })
+})
+
+describe('Finding elements of movie list', () => {
+  it('should find all movies containing \'Hello world\'', () => {
+    cy.get('input').type('Hello world');
+
+    cy.get('button').click();
+
+    cy.get('h3').each((el) => { 
+      cy.wrap(el).contains('hello', { matchCase: false }).should('exist');
+      cy.wrap(el).contains('world', { matchCase: false }).should('exist');
+    });
   })
 })
